e76fc7688c1c493911f1668223632b10052e7979
Vimalnath1 <61100702+Vimalnath1@users.noreply.github.com>
2021-10-20 18:52:12 -0400

    Update kivything.py

--- kivything.py
+++ kivything.py
@@ -22,7 +22,7 @@ from kivy.lang import Builder
 import time
 from threading import *
 iffalse=True
-def recipeappfunction():
+def recipeappfunction(): #main logic go to line 41 where it actually starts
     global recipe_labels, iftrue
     def jsonStripper(thingToStrip,whatToStripInStrip):
         data =json.loads(thingToStrip)
@@ -34,16 +34,16 @@ def recipeappfunction():
         return dataToPrint
     url = "https://edamam-recipe-search.p.rapidapi.com/search"
     i=True
-    food={}
+    food={}                                             
     #cam=cv2.VideoCapture("http://192.168.1.4:8080/video")
-    cam=cv2.VideoCapture(0)
+    cam=cv2.VideoCapture(0)                                         #makes the camera pop up for the scanning of barcode
 
     conn = http.client.HTTPSConnection("edamam-food-and-grocery-database.p.rapidapi.com")
     headers = {
         'x-rapidapi-host': "edamam-food-and-grocery-database.p.rapidapi.com",
         'x-rapidapi-key': "da66f99cf6msh0256ef4567f1ffbp1ea5dejsn972563a3211b"
         }
-
+                                                                                    #APIs used 
     recipeheaders = {
         'x-rapidapi-host': "edamam-recipe-search.p.rapidapi.com",
         'x-rapidapi-key': "da66f99cf6msh0256ef4567f1ffbp1ea5dejsn972563a3211b"
@@ -55,7 +55,7 @@ def recipeappfunction():
         for barcode in decode(frame):
             if i==True:
                 labeldata=barcode.data.decode("utf-8")
-                conn.request("GET", f"/parser?upc={labeldata}", headers=headers)
+                conn.request("GET", f"/parser?upc={labeldata}", headers=headers)        #Scanning Barcode Logic Commented out because testing and too lazy to scan every time
                 res = conn.getresponse()
                 data = res.read()
                 food_info=data.decode()
@@ -65,14 +65,14 @@ def recipeappfunction():
         if cv2.waitKey(10)==ord("q"):
             break
         cv2.imshow("Camera", frame)'''
-    conn.request("GET", f"/parser?upc=0737628005000", headers=headers)
+    conn.request("GET", f"/parser?upc=0737628005000", headers=headers)      #sample barcode for testing 
     res = conn.getresponse()
     data = res.read()
     food_info=data.decode()
     where_label=food_info.find("label")
     where_nutrients=food_info.find("nutrients")
-    where_calorie=food_info.find("ENERC_KCAL")
-    where_fat=food_info.find("FAT")
+    where_calorie=food_info.find("ENERC_KCAL")              #When barcode scans prints out huge string, the where variables find each nutrient and the food variables
+    where_fat=food_info.find("FAT")                         #break it apart
     where_saturatedfat=food_info.find("FASAT")
     where_protein=food_info.find("PROCNT")
     where_sugar=food_info.find("SUGAR")
@@ -94,10 +94,10 @@ def recipeappfunction():
     food_sodium=food_info[where_sodium:where_calcium]
     food_brand=food_info[where_brand:where_category]
     if "brand" in food_brand:
-        food_brand=food_brand.replace('''brand" :''',"")
+        food_brand=food_brand.replace('''brand" :''',"")            
     if '''"''' in food_brand:
         food_brand=food_brand.replace('''"''',"")
-        food_brand=food_brand.lstrip()
+        food_brand=food_brand.lstrip()                      #To clean out the food label because it will go into api and it cant have spaces or quotations
         food_brand=food_brand.rstrip()
     if food_brand in food_label:
         food_label=food_label.replace(food_brand,"")
@@ -109,7 +109,7 @@ def recipeappfunction():
     food["Name"]=food_label
     food["Calories"]=food_calories
     food["Fat"]=food_fat
-    food["Protein"]=food_protein
+    food["Protein"]=food_protein        #Adding the each nutrient part to dictionary on line 37
     food["Fiber"]=food_fiber
     food["Carbohydrates"]=food_carbs
     food["Sugar"]=food_sugar
@@ -120,20 +120,20 @@ def recipeappfunction():
     pprint.pprint(food)
     querystring = {"q":f"{food_label}"}
 
-    response = requests.request("GET", url, headers=recipeheaders, params=querystring)
+    response = requests.request("GET", url, headers=recipeheaders, params=querystring)  #calling second api to look up the food label in it to find recipes
     recipes=response.text
     #pprint.pprint(recipes)
     #find_where_all_substrings_are(recipes,'''"uri":''')
     #print(type(recipes))
     recipe_labels=jsonStripper(recipes,"label")
-    recipe_calories=jsonStripper(recipes,"calories")
-    recipe_health=jsonStripper(recipes,"healthLabels")
+    recipe_calories=jsonStripper(recipes,"calories")                                    
+    recipe_health=jsonStripper(recipes,"healthLabels")              #is also returned as a long string so jsonStripper(lines 27-34) breaks it 
     recipe_cuisine=jsonStripper(recipes,"cuisineType")
     recipe_allergens=jsonStripper(recipes,"cautions")
     recipe_ingredients=jsonStripper(recipes,"ingredientLines")
     iftrue=True
 class HomeButton(Button,ActionItem):
-    pass
+    pass                                    #Each class is are the screens of the app
 class MenuBar(ActionBar):
     pass
 class MainWindow(Screen):
@@ -145,51 +145,18 @@ class RecipeWindow(Screen):
         recipeappfunction()
 if iffalse==False:
     class RecipeList(Screen):
-        def __init__(self, **kwargs):
+        def __init__(self, **kwargs):                           
             super(RecipeList,self).__init__(**kwargs)
             layout=BoxLayout(orientation="vertical")        
             self.add_widget(layout)
-            try:
-                for i in range(len(recipe_labels)):
-                    b=Button(text=recipe_labels[i],size_hint=(1,1))
-                    self.add_widget(b) 
-            except NameError:
+            try:                                                        #Here is my main problem. recipe labels is declared in the function at line 128. Only turns global at 145
+                for i in range(len(recipe_labels)):                     #This for loop creates a button for each label in the list, the thing is if I declare recipe_labels=[]
+                    b=Button(text=recipe_labels[i],size_hint=(1,1))     # it will put zero buttons and keep it like that even if recipe_labels changes after the function
+                    self.add_widget(b)                                  #I want this to basically not exist until line 144
+            except NameError:                                          
                 pass
 class WindowManager(ScreenManager):
     pass
-class WidgetforApp(GridLayout):
-    def on_food_tracker_button_click(self):
-        #clear page
-        print("Clear Page and ask about nutrition goals and ask to scan and add to food tracker")
-    def on_recipes_button_click(self):
-        #clear page
-        print("Clear Page and ask to scan food and then show recipes")
-class WidgetsExample(GridLayout):
-    number=0
-    def on_button_click(self):
-        self.number+=1
-        print(self.number)
-    def security_camera(self):
-            cam=cv2.VideoCapture(0)
-            while cam.isOpened():
-                ret, frame1=cam.read()
-                ret, frame2=cam.read()
-                diff=cv2.absdiff(frame1,frame2)
-                gray=cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
-                blur=cv2.GaussianBlur(gray,(5,5),0)
-                _,thresh=cv2.threshold(blur, 20,255, cv2.THRESH_BINARY)
-                dilate=cv2.dilate(thresh, None, iterations=3)
-                contours,_=cv2.findContours(dilate,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-                for c in contours:
-                    if cv2.contourArea(c)<5000:
-                        continue
-                    x,y,w,h=cv2.boundingRect(c)
-                    cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
-                    winsound.PlaySound("alarm.wav", winsound.SND_ASYNC)
-                if cv2.waitKey(10)==ord("q"):
-                    break
-                cv2.imshow("Camera", frame1)
-
 
 kv=Builder.load_file("My.kv")
 class MyApp(App):
